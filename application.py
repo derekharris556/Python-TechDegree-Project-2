5347dc1331f166f35005188eab8efd5a303765b2
derekharris556 <164573588+derekharris556@users.noreply.github.com>
2024-10-04 21:24:03 -0500

    Add files via upload

--- application.py
+++ application.py
@@ -5,16 +5,19 @@ import constants
 # Save player height as int, experience as True/False, guardians as list of str.
 # Append clean_players to new list
 def clean_data():
+    players = copy.deepcopy(constants.PLAYERS)
     cleaned_players = []
-    for player in constants.PLAYERS:
-        clean_player = copy.deepcopy(player)
-        clean_player['height'] = int(player['height'].split()[0])
-        if clean_player['experience'] == 'YES':
-            clean_player['experience'] = True
+    
+    for player in players:
+        player['height'] = int(player['height'].split()[0])
+        if player['experience'] == 'YES':
+            player['experience'] = True
         else:
-            clean_player['experience'] = False
-        clean_player['guardians'] = clean_player['guardians'].split(' and ')    
-        cleaned_players.append(clean_player)
+            player['experience'] = False
+            
+        player['guardians'] = player['guardians'].split(' and ')    
+        cleaned_players.append(player)
+        
     return cleaned_players
 
 
@@ -22,25 +25,43 @@ def clean_data():
 def sort_players_by_experience():
     experienced_players = []
     inexperienced_players = []
+
     for player in cleaned_players:
         if player['experience'] == True:
             experienced_players.append(player)
         elif player['experience'] == False:
             inexperienced_players.append(player)
+            
     return experienced_players, inexperienced_players
 
             
-#TODO: Assign players equally to the 3 teams, ensure that teams have equal num experieneced players
-def balance_teams():
-    teams = {team: [] for team in copy.deepcopy(constants.TEAMS)}
-    num_teams = len(teams)
-    num_experienced_players = len(experienced_players)
-    num_inexperienced_players = len(inexperienced_players)
-    for team in teams:
-        for experienced_players in range(num_experienced_players):
-            teams[team].append(experienced_players.pop(0))
+# Assign players equally to the 3 teams, ensure that teams have equal num experieneced players
+def balance_teams(experienced_players, inexperienced_players):
+    teams = copy.deepcopy(constants.TEAMS)
+
+    # Calculate players per team
+    experienced_per_team = len(experienced_players) // len(teams)
+    inexperienced_per_team = len(inexperienced_players) // len(teams)
+
+    # Initialize team names
+    team_names = ["Panthers", "Bandits", "Warriors"]
+
+    # Round-robin assignment of experienced players using dictionary comprehension
+    team_rosters = {team: [] for team in team_names}
+
+    team_rosters = {team: [experienced_players[i] for i in range(len(experienced_players)) if i % len(team_names) == idx]
+                    for idx, team in enumerate(team_names)}
+
+    # Round-robin assignment of inexperienced players
+    for idx, player in enumerate(inexperienced_players):
+        team_name = team_names[idx % len(teams)]
+        team_rosters[team_name].append(player)
+
+    return team_rosters["Panthers"], team_rosters["Bandits"], team_rosters["Warriors"]
+
     
-        return teams
-
-
 #TODO: Create a function to find the average height of each team
+def average_height(team_rosters):
+    
+
+
